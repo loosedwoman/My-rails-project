0a3cf18684caa9915eda2a0bfd56959a4a24f7b2
amaka <aechido@yahoo.com>
2017-02-15 18:19:39 +0900

    3.7 first commit

---
+++ js/work.js
@@ -0,0 +1,24 @@
+var works=[
+{
+ title: "First Project", 
+ pic:"img/sunrise hike.jpg",
+ url:"img/sunrise hike.jpg"
+},
+{
+ title: "second project", 
+ pic:"img/dancer.jpg",
+ url:"img/dancer.jpg"
+},
+{ 
+ title:"third project", 
+ pic:"img/butterflywall.jpg",
+ url:"img/butterflywall.jpg"
+},
+{
+title: "fourth project", 
+pic:"img/butterflies.jpg",
+url:"img/butterflies.jpg"
+}
+];
+
+//change images to align properly
